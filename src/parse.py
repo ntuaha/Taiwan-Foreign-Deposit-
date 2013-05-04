1572645f0c7b7d3d311c98f9e14388f924c53f2f
Cheng-Yu Lin <ntuaha@gmail.com>
2013-05-04 21:42:54 +0800

    第一版完成

--- src/parse.py
+++ src/parse.py
@@ -2,27 +2,22 @@
 
 import xlrd
 import re
+
+#處理掉unicode 和 str 在ascii上的問題
 import sys 
+import os
 reload(sys) 
 sys.setdefaultencoding('utf8') 
 
 def parse(source_path,destination_path,date):
-	f = open(destination_path+date+"_raw.csv","w+")
 	book = xlrd.open_workbook(source_path+date+".xls")
 	print "The number of worksheets is",book.nsheets
 	print "Worksheet name(s):", book.sheet_names()
 	sh = book.sheet_by_index(0)
 	print sh.name, sh.nrows, sh.ncols
-	#for i in range(sh.nrows):
-#		print "Line %d" %i	
-#		print " ".join([unicode(sh.cell_value(rowx=i,colx=j)) for  j in range(sh.ncols)])
-		#for j in range(sh.ncols):
-		#	print sh.cell_value(rowx=i,colx=j)
-	#fopen
-	header = ['年月','銀行','銀行英文','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額','金控註記']
-#	print header
 
 	bank_data = {}
+	header = ['年月','銀行','銀行英文','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額','金控註記']
 	total_data = range(len(header)-3)
 	jump_gap = 8
 	mode  = 0
@@ -69,7 +64,7 @@ def parse(source_path,destination_path,date):
 			bank_data[bank_name]["ALL_Y"] = float(sh.cell_value(rowx=i,colx = 3))*1e6
 
 
-		#全行總和
+		#國內總和
 		if sh.cell_value(rowx=i,colx = 0)== u"總　　　　　計　Total" and 2 == mode:
 			#國內
 			total_data[4] = float(sh.cell_value(rowx=i,colx = 1))*1e6
@@ -80,7 +75,7 @@ def parse(source_path,destination_path,date):
 			total_data[8] = total_data[2] - total_data[5]
 			total_data[9] = total_data[3] - total_data[6]
 			continue
-		#全行銀行
+		#國內銀行
 		if 2 == mode:
 			bank_name = unicode(sh.cell_value(rowx=i,colx = 0))
 			bank_data[bank_name]["DB_MY"] = float(sh.cell_value(rowx=i,colx = 1))*1e6
@@ -91,10 +86,12 @@ def parse(source_path,destination_path,date):
 			bank_data[bank_name]["OS_Y"] = bank_data[bank_name]["ALL_Y"] - bank_data[bank_name]["DB_Y"]
 				#print "%s %% %s" %(bank_data[bank_name],bank_name)
 
+	output(destination_path,date,total_data,bank_data,header)
 				
 
 #輸出
-	f = open("%sTotal_%s.csv"%(destination_path,date),"w+")
+def output(destination_path,date,total_data,bank_data,header):
+	f = open("%sTotal.csv"%(destination_path),"a+")
 	
 	total_header = ['年月','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額']
 	f.write(",".join(total_header)+"\n")
@@ -102,13 +99,15 @@ def parse(source_path,destination_path,date):
 	f.close()
 
 	f = open("%s%s.csv"%(destination_path,date),"w+")
-	header = ['年月','銀行','銀行英文','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額','金控註記']
+	#header = ['年月','銀行','銀行英文','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額','金控註記']
 	f.write(",".join(header)+"\n")
 	for i in bank_data:
 		try:
 			d = [None]*13
+			#print "%s,%d"% (i,len(re.findall("#",i)))
+			#print re.split('[\W+|(]', i, flags=re.U)[0]
 			d[0] = date
-			d[1] = i
+			d[1] = re.split('[\W+|(]', i, flags=re.U)[0]
 			d[2] = ""
 			d[3] = int(bank_data[i]["ALL_MY"])
 			d[4] = int(bank_data[i]["ALL_FY"])
@@ -119,7 +118,7 @@ def parse(source_path,destination_path,date):
 			d[9] = int(bank_data[i]["OS_MY"])
 			d[10] = int(bank_data[i]["OS_FY"])
 			d[11] = int(bank_data[i]["OS_Y"])
-			d[12] = 0
+			d[12] = len(re.findall("#",i))
 			f.write(",".join(map(str,d))+"\n")
 		except KeyError:
 			f.write(",".join(map(str,d))+"\n")
@@ -133,4 +132,12 @@ def parse(source_path,destination_path,date):
 
 
 if __name__ == '__main__':
-	parse('/Users/aha/Dropbox/Project/Financial/Data/','','9501')
+	from_path= '/Users/aha/Dropbox/Project/Financial/Data/'
+	to_path = '/Users/aha/Dropbox/Project/Financial/Codes/'
+
+	yy = 95
+	mm = 1
+	date = '%d%02d'%(yy,mm)
+	cmd = "rm %sTotal.csv"%(to_path)
+	os.system(cmd)
+	parse(from_path,to_path,date)
