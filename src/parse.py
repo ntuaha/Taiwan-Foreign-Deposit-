a0ab0054b5bb3f3de03e993c0b8dbbf2e3d76d8c
Cheng-Yu Lin <ntuaha@gmail.com>
2013-05-04 22:29:25 +0800

    第一版完成

--- src/parse.py
+++ src/parse.py
@@ -93,9 +93,8 @@ def parse(source_path,destination_path,date):
 def output(destination_path,date,total_data,bank_data,header):
 	f = open("%sTotal.csv"%(destination_path),"a+")
 	
-	total_header = ['年月','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額']
-	f.write(",".join(total_header)+"\n")
-	f.write(",".join(map(str,map(int,total_data))))
+
+	f.write(",".join(map(str,map(int,total_data)))+"\n")
 	f.close()
 
 	f = open("%s%s.csv"%(destination_path,date),"w+")
@@ -133,11 +132,20 @@ def output(destination_path,date,total_data,bank_data,header):
 
 if __name__ == '__main__':
 	from_path= '/Users/aha/Dropbox/Project/Financial/Data/'
-	to_path = '/Users/aha/Dropbox/Project/Financial/Codes/'
-
-	yy = 95
-	mm = 1
-	date = '%d%02d'%(yy,mm)
+	to_path = '/Users/aha/Dropbox/Project/Financial/Codes/csv/'
 	cmd = "rm %sTotal.csv"%(to_path)
 	os.system(cmd)
-	parse(from_path,to_path,date)
+
+	f = open("%sTotal.csv"%(to_path),"a+")
+	total_header = ['年月','全行外匯活期存款','全行外匯定期存款','全行總額','國內外匯活期存款','國內外匯定期存款','國內總額','海外外匯活期存款','海外外匯定期存款','海外總額']
+	f.write(",".join(total_header)+"\n")
+	f.close()
+	yy = 95
+	mm = 1
+	for yy in range(95,100):
+		for mm in range(1,13):
+			date = '%d%02d'%(yy,mm)
+			parse(from_path,to_path,date)
+	parse(from_path,to_path,'10001')
+
+
